4359504a19d9f23e642093be45e981aae2dd3e9a
Rushabh Modi <rushabhmodi2552@gmail.com>
2023-04-05 23:24:55 +0530

    home page update

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,20 @@
 import React from "react";
 import HeroSection from "./components/HeroSection";
+import Trusted from "./components/Trusted";
+import Services from "./components/Services";
 
 const Home = () => {
   const data = {
     name: "EchoEcommerce",
   };
-  
-  return <HeroSection myData={data} />;
+
+  return (
+    <>
+      <HeroSection myData={data} />;
+      <Services />
+      <Trusted />
+    </>
+  );
 };
 
 export default Home;
