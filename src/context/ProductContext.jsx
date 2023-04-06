44533d87a38357027ea74cbba542e19cb0838127
Rushabh Modi <rushabhmodi2552@gmail.com>
2023-04-06 14:20:03 +0530

    useReducer Hooks

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,24 +1,38 @@
 import axios from "axios";
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
+import reducer from "../reducer/ProductReducer";
 
 const AppContext = createContext();
 
 const API = "https://api.pujakaitem.com/api/products";
 
+const initialState = {
+  isLoading: false,
+  isError: false,
+  products: [],
+  featureProducts: [],
+};
+
 const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   const getProducts = async (url) => {
-    const res = await axios.get(url);
-    const Products = await res.data;
-    console.log("🚀 ~ file: ProductContext.jsx:12 ~ getProducts ~ Products:", Products)
+    dispatch({ type: "SET_LOADING" });
+    try {
+      const res = await axios.get(url);
+      const products = await res.data;
+      dispatch({ type: "SET_API_DATA", payload: products });
+    } catch (error) {
+      dispatch({ type: "API_ERROR" });
+    }
   };
+
   useEffect(() => {
     getProducts(API);
   }, []);
 
   return (
-    <AppContext.Provider value={{ myName: "Rushabh Modi" }}>
-      {children}
-    </AppContext.Provider>
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
   );
 };
 
