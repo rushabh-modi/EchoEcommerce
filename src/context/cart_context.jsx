41779b7b1139c6f00b28c796d2c74b129dc68ebf
Rushabh Modi <rushabhmodi2552@gmail.com>
2023-04-15 18:06:44 +0530

    Clear Cart Button & continue Shopping

--- src/context/cart_context.jsx
+++ src/context/cart_context.jsx
@@ -1,10 +1,21 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useReducer, useEffect } from "react";
 import reducer from "../reducer/cartReducer";
 
 const CartContext = createContext();
 
+//localstorage to not empty cart section while refreshing the page
+const getLocalCartData = () => {
+  let localCartData = localStorage.getItem("LSCart");
+  if (localCartData === []) {
+    return [];
+  } else {
+    return JSON.parse(localCartData);
+  }
+};
+
 const initialState = {
-  cart: [],
+  //   cart: [],
+  cart: getLocalCartData(),
   total_item: "",
   total_amount: "",
   shipping_fee: 50000,
@@ -21,8 +32,20 @@ const CartProvider = ({ children }) => {
     dispatch({ type: "REMOVE_ITEM", payload: id });
   };
 
+  //to clear cart
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
+
+  //   to add the data in local Storage
+  //get vs set
+
+  useEffect(() => {
+    localStorage.setItem("LSCart", JSON.stringify(state.cart));
+  }, [state.cart]);
+
   return (
-    <CartContext.Provider value={{ ...state, addToCart, removeItem }}>
+    <CartContext.Provider value={{ ...state, addToCart, removeItem, clearCart }}>
       {children}
     </CartContext.Provider>
   );
--- src/reducer/cartReducer.jsx
+++ src/reducer/cartReducer.jsx
@@ -1,40 +1,47 @@
 const cartReducer = (state, action) => {
-    if (action.type === "ADD_TO_CART") {
-      let { id, color, amount, product } = action.payload;
+  if (action.type === "ADD_TO_CART") {
+    let { id, color, amount, product } = action.payload;
     //   console.log(
     //     "🚀 ~ file: cartReducer.js ~ line 4 ~ cartReducer ~ product",
     //     product
     //   );
-  
-      let cartProduct;
-  
-      cartProduct = {
-        id: id + color,
-        name: product.name,
-        color,
-        amount,
-        image: product.image[0].url,
-        price: product.price,
-        max: product.stock,
-      };
-      
-      return {
-        ...state,
-        cart: [...state.cart, cartProduct],
-      };
-    }
-  
-    if (action.type === "REMOVE_ITEM") {
-      let updatedCart = state.cart.filter(
-        (curItem) => curItem.id !== action.payload
-      );
-      return {
-        ...state,
-        cart: updatedCart,
-      };
-    }
-  
-    return state;
-  };
-  
-  export default cartReducer;
+
+    let cartProduct;
+
+    cartProduct = {
+      id: id + color,
+      name: product.name,
+      color,
+      amount,
+      image: product.image[0].url,
+      price: product.price,
+      max: product.stock,
+    };
+
+    return {
+      ...state,
+      cart: [...state.cart, cartProduct],
+    };
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    let updatedCart = state.cart.filter(
+      (curItem) => curItem.id !== action.payload
+    );
+    return {
+      ...state,
+      cart: updatedCart,
+    };
+  }
+
+  //to empty cart
+  if (action.type === "CLEAR_CART") {
+    return {
+      ...state,
+      cart: [],
+    };
+  }
+  return state;
+};
+
+export default cartReducer;
